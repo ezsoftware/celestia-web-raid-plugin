f17d997f0646b57e8f949bac9b6c4684ff2e0fcd
John Ryan <john@ezsoftwareinc.com>
2017-05-15 22:07:47 -0700

    plugin meta

--- celestia-web-raid.php
+++ celestia-web-raid.php
@@ -1,2 +1,10 @@
 <?php
-  defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+/**
+ * Plugin Name: Raid Manager
+ * Description: Custom Raid Manager by Celestia FC on Hyperion
+ * Version: 0.1 (05/15/2017)
+ * Author: John Ryan - EZ Software Inc
+ * Author URI: http://www.example.com
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
